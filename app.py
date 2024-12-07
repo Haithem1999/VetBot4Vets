3f77d9f8bf60d5152c402af1d392c0af106883a8
Haithem1999 <41543751+Haithem1999@users.noreply.github.com>
2024-12-07 22:09:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,8 @@ def generate_response(prompt):
 
     response = client.chat.completions.create(
         #model="ft:gpt-4o-mini-2024-07-18:personal::ATCwbTAA",
-        model = "ft:gpt-4o-mini-2024-07-18:personal::Aa7eN5z8",
+        #model = "ft:gpt-4o-mini-2024-07-18:personal::Aa7eN5z8",
+        model = "gpt-4o-mini-2024-07-18",
         messages=[{"role": "system", "content": system_prompt}] + st.session_state.messages + [{"role": "user", "content": user_prompt}],
     )
     
